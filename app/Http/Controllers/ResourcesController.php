f0d8b47e295795da510a0d21b456ff68802ac00b
Analia Mok <axm2580@g.rit.edu>
2019-01-20 19:38:51 -0500

    Manually limiting entries. Cards now responsive

--- app/Http/Controllers/ResourcesController.php
+++ app/Http/Controllers/ResourcesController.php
@@ -23,24 +23,23 @@ class ResourcesController extends Controller
         // TODO: Encapsulate into helper class.
         $query = new \Contentful\Delivery\Query();
         $query->setContentType('whitePaper')
-            ->orderBy('sys.updatedAt')
-            ->setInclude(3);
+            ->orderBy('sys.updatedAt');
         $white_paper_entries = $this->client->getEntries($query);
+
         $white_papers = [];
 
-        // foreach ($white_paper_entries as $entry) {
-        //     $white_papers[] = [
-        //         'title'         => $entry['title'],
-        //         'slug'          => $entry->getSlug(),
-        //         'header'        => $entry->getContentHeader(),
-        //         'summary'       => $entry->getSummary(),
-        //         'pdf'           => $entry->getPdf(),
-        //         'featuredImg'   => $entry->getFeaturedImage(),
-        //     ];
-        // }
+        foreach ($white_paper_entries as $wp) {
+            if (count($white_papers) >= 3) {
+                break;
+            }
+
+            $white_papers[] = $wp;
+        }
+
+        // TODO: Manually limit Contentful Resource Array
 
         return view('resources.index', [
-            'white_papers' => $white_paper_entries,
+            'white_papers' => $white_papers,
             'renderer'     => $renderer = new \Contentful\RichText\Renderer(),
         ]);
     }
--- resources/views/resources/index.blade.php
+++ resources/views/resources/index.blade.php
@@ -2,22 +2,31 @@
 
 @section('body')
 <h1>Resources</h1>
-<div class="row">
+<h3>White Papers</h3>
+<div class="row card__container">
     @foreach($white_papers as $entry)
-    <div class="col-xs-12 col-md-4 card">
-        <img src="{{ $entry->featuredImage->getFile()->getUrl() }}"
-             alt="{{ $entry->title }}" class="card-img-top">
-        <div class="card-body">
-            <span class="card__category">CASE STUDY</span>
-            <h3 class="card__title">{{ $entry->title }}</h3>
-            {{-- <div class="card__content">{!! $entry->summary !!}</div> --}}
-            @php
-                $output = $renderer->render($entry->summary);
-                echo str_limit(strip_tags($output), 250);
-            @endphp
-            <a href="/white-paper/{{ $entry->slug }}" class="card__link">Read More</a>
+    <div class="col-xs-12 col-md-6 col-lg-4">
+        <div class="card">
+            <img src="{{ $entry->featuredImage->getFile()->getUrl() }}"
+            alt="{{ $entry->title }}" class="card-img-top">
+            <div class="card-body">
+                <span class="card__category">WHITE PAPER</span>
+                <h3 class="card__title">{{ $entry->title }}</h3>
+                <div class="card__content">
+                    @php
+                    $output = $renderer->render($entry->summary);
+                    echo str_limit(strip_tags($output), 250);
+                    @endphp
+                </div>
+                <a href="/white-paper/{{ $entry->slug }}" class="card__link">Read More</a>
+            </div>
         </div>
     </div>
     @endforeach
+</div>
+<div class="row">
+    <div class="col-xs-12 col-lg-4 offset-lg-4 card__container__button-holder">
+        <a href="/case-studies" class="button">See All Case Studies</a>
+    </div>
 </div>
 @endsection
