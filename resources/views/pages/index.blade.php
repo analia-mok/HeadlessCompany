c8531d2b80bd495c7e74e7597106ec993affa0c0
Analia Mok <axm2580@g.rit.edu>
2019-01-21 20:52:01 -0500

    Added temp links to home page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,7 +5,7 @@
 @section('body')
 <h1>Home Page</h1>
 <p>Under construction</p>
-<h2>Main Pages Implemented</h2>
+<h2>Implemented Pages</h2>
 <ul>
     <li><a href="/resources">Resources</a></li>
     <li><a href="/white-papers">White Papers</a></li>
