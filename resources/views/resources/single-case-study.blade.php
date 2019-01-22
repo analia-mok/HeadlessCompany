e494e1435481a4ceb5e6f81b568c1ca948fd4520
Analia Mok <axm2580@g.rit.edu>
2019-01-21 20:04:13 -0500

    Now displaying highlights

--- resources/views/resources/single-case-study.blade.php
+++ resources/views/resources/single-case-study.blade.php
@@ -11,6 +11,14 @@
 <div class="row">
     <div class="col-xs-12 col-md-12">
         <img src="{{ $entry->featuredImage->getFile()->getUrl() }}" alt="{{ $entry->title }}" class="img-fluid single__img">
+        @if(count($entry->highlightedExpertise) > 0)
+        <h2 class="single__subheader">Highlighted Expertise</h2>
+        <ul class="single__highlights">
+            @foreach($entry->highlightedExpertise as $exp)
+                <li class="single__highlights__item">{{ $exp }}</li>
+            @endforeach
+        </ul>
+        @endif
         <div class="single__content">{!! $renderer->render($entry->content) !!}</div>
     </div>
 </div>
