37702422078de928b1ce80f123514f8f6caf6f9f
Analia Mok <axm2580@g.rit.edu>
2019-01-22 20:08:29 -0500

    Implemented download form

--- resources/views/components/download-form.blade.php
+++ resources/views/components/download-form.blade.php
@@ -1,6 +1,19 @@
 <div class="download-form">
-    <strong class="download-form__title">{{ $slot }}</strong>
+    <h3 class="download-form__title">{{ $slot }}</h3>
     <form method="POST" action="#">
-        {{-- TODO --}}
+        @csrf
+        <input type="text" name="first_name" placeholder="First Name" required>
+        <input type="text" name="last_name" placeholder="Last Name" required>
+        <input type="email" name="email" placeholder="Email" required>
+        <input type="tel" name="phone" placeholder="Phone">
+        <input type="text" name="company" placeholder="Company">
+        <select name="job_title" id="job_function" class="dropdown">
+            <option value>Job Function</option>
+            <option value="businessex">Business Executive</option>
+            <option value="marketing">Marketing</option>
+            <option value="it">Technology/ IT</option>
+            <option value="developer">Developer</option>
+        </select>
+        <input type="submit" value="Download Now" class="button">
     </form>
 </div>
--- resources/views/resources/white-papers/show.blade.php
+++ resources/views/resources/white-papers/show.blade.php
@@ -3,11 +3,25 @@
 
 @section('body')
 <h1 class="page-title">{{ $entry->title }}</h1>
-<div class="row">
-    <div class="col-xs-12 col-md-12">
+
+<div class="row single">
+    <div class="col-xs-12 col-md-7">
         <img src="{{ $entry->featuredImage->getFile()->getUrl() }}" alt="{{ $entry->title }}" class="img-fluid single__img">
-        <div class="single__content">{!! $renderer->render($entry->summary) !!}</div>
-        {{-- TODO: Structure to match design --}}
+        <div class="single__content">
+            @if(!empty($entry->contentHeader))
+                <h2 class="single__subheader">{{ $entry->contentHeader }}</h2>
+            @endif
+            {!! $renderer->render($entry->summary) !!}
+        </div>
+    </div>
+    <div class="col-xs-12 col-md-5">
+        @component('components.download-form')
+            @if(!empty($entry->downloadFormTitle))
+                {{ $entry->downloadFormTitle }}
+            @else
+                Read more about this publication
+            @endif
+        @endcomponent
     </div>
 </div>
 @endsection
