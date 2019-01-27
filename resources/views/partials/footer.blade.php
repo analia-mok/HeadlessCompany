fd93901c68f30e36fee24d5be7157650f77cc136
Analia Mok <axm2580@g.rit.edu>
2019-01-27 13:30:08 -0500

    Implemented footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,21 @@
-<footer></footer>
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-md-12">
+                <ul class="footer__links">
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/resources">Resources</a></li>
+                    <li><a href="/blog">Blog</a></li>
+                    <li><a href="/careers">Careers</a></li>
+                    <li><a href="/contact-is">Contact Us</a></li>
+                </ul>
+                <p>Copyright &copy; 2019 Headless Company</p>
+                <ul class="footer__social-links">
+                    <li><object type="image/svg+xml" data="/svg/twitter.svg" class="logo">Twitter Logo</object></li>
+                    <li><object type="image/svg+xml" data="/svg/linkedin.svg" class="logo">LinkedIn Logo</object></li>
+                    <li><object type="image/svg+xml" data="/svg/instagram.svg" class="logo">Instagram Logo</object></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
